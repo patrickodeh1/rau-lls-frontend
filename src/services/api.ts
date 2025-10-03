7bea98fc9afc647a55fa44ca611616cb19fbdfdb
Patrick <vyltyan@gmail.com>
2025-10-03 20:31:52 +0100

    Update API base URL in .env and api.ts, modify title in index.html, and remove unused assets

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000/api/",
+  baseURL: process.env.REACT_APP_API_URL || "https://raulls.revenueaccelerationunit.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
